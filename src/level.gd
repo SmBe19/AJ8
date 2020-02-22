1a50c091eeda831eb49df2ecfdfb164a542ea173
mad-s <13462849+mad-s@users.noreply.github.com>
2020-02-22 23:04:49 +0100

    new scene

---
+++ src/level.gd
@@ -0,0 +1,21 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+export(Material) var material
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for child in get_children():
+		if child is MeshInstance:
+			for i in child.get_surface_material_count():
+				print(child, i)
+				child.set_surface_material(i, self.material)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
